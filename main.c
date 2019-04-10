0f31141d97eaa4513d73b712df93b8c2f645d21a
Mariana <marianaplazas@live.com>
2019-04-10 23:15:11 +0000

    [fixes] corrections for the code and frist function of path

--- main.c
+++ main.c
@@ -6,16 +6,18 @@ int main(void)
 	char *line = NULL;
 	size_t len = 0;
 	char **token;
+/*	int status;
+ */
 	
 	while (1) 
 	{
 		printf("$ ");
-		getline(&line, &len, stdin);
-		token = _strtok(line); 
-		//	printf("%s", token[3]);
-// agregar el nul luego del srtok
+		/* status =*/ getline(&line, &len, stdin);
+		token = _strtok(line);
+		if (token[0] == NULL)
+			perror("no such a file or directory");
+		path();
 	}
-
 	free(line);
 	exit(98);
 	return(0);
--- shell.h
+++ shell.h
@@ -1,4 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 char **_strtok(char *istring);
+int hand_errors(char **av);
+void path();
+int cont_word(char * palabra);
 #endif
